aaf34340ccc274bee1bb76fa0df392529680e13b
Your Name <you@example.com>
2024-07-27 17:37:19 +0800

    WebServer

---
+++ build/CMakeFiles/WebServer.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/baiyu/myWebServer/http_conn.cpp" "CMakeFiles/WebServer.dir/http_conn.cpp.o" "gcc" "CMakeFiles/WebServer.dir/http_conn.cpp.o.d"
+  "/home/baiyu/myWebServer/log.cpp" "CMakeFiles/WebServer.dir/log.cpp.o" "gcc" "CMakeFiles/WebServer.dir/log.cpp.o.d"
+  "/home/baiyu/myWebServer/main.cpp" "CMakeFiles/WebServer.dir/main.cpp.o" "gcc" "CMakeFiles/WebServer.dir/main.cpp.o.d"
+  "/home/baiyu/myWebServer/sql_connection_pool.cpp" "CMakeFiles/WebServer.dir/sql_connection_pool.cpp.o" "gcc" "CMakeFiles/WebServer.dir/sql_connection_pool.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/WebServer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WebServer.dir/http_conn.cpp.o"
+  "CMakeFiles/WebServer.dir/http_conn.cpp.o.d"
+  "CMakeFiles/WebServer.dir/log.cpp.o"
+  "CMakeFiles/WebServer.dir/log.cpp.o.d"
+  "CMakeFiles/WebServer.dir/main.cpp.o"
+  "CMakeFiles/WebServer.dir/main.cpp.o.d"
+  "CMakeFiles/WebServer.dir/sql_connection_pool.cpp.o"
+  "CMakeFiles/WebServer.dir/sql_connection_pool.cpp.o.d"
+  "WebServer"
+  "WebServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WebServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
